45449f6aa1d314a6d9d86aeab1ec2306b2da993f
Leandro Curzi <lcurzi@MODUSAGENCY.COM>
2018-02-06 19:52:15 -0300

    -

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 let indentString = require("indent-string")
 
 class Activity{
-    static get(id){
-        if(!id || id.trim().length == 0) return null;
+    /**
+     * Generic handler for all GET requests in this service
+     * @param {*} query = {
+     *      type: String,
+     *      param: Object
+     * } 
+     */
+    static get(query){
+        let result = {status: false, resp: null};
+
+        switch(query.type){
+            case "GET_BY_ID": 
+                result.resp = this.getById(query.param);
+                result.status = true;
+            break;
+
+            case "GET_BY_TYPE":
+                result.resp = this.getByType(query.param);
+                result.status = true;
+            break;
+        }
+
+        return result;
 
         return {
             activity_id: "1234679800",
             type: indentString('Unicorns Rainbows', 4),
         }
+    }
+
+    static getById(id){
+        return {activity_id: "1234679800", type: indentString('Type 1', 4)};
+    }
+
+    static getByType(type){
+        return {activity_id: "1234679800", type: indentString('Type 2', 8)};
     }
 }
 
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 let Activity = require("./index");
 
-let mockData = Activity.get("1");
+let mockData = Activity.get({type:"GET_BY_ID", param: "abc123"});
 console.log(mockData);
